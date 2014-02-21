b1253b7dddadd717ad05e2a270f0b72b9b461e03
Christopher Lillthors <lillt@kth.se>
2014-02-21 11:23:50 +0100

    Initial commit

---
+++ Uppgift8/test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+	"log"
+)
+------------------------------------------------------------
+const (
+	numberOfHello = 1000
+)
+
+func sayHello(ch chan string, wg *sync.WaitGroup) {
+	for {
+		select {
+		case input := <-ch:
+			fmt.Println(input)
+			wg.Done()
+		}
+	}
+}
+
+func main() {
+	wg := new(sync.WaitGroup)
+	ch := make(chan string)
+	wg.Add(numberOfHello)
+	for i := 1; i <= numberOfHello; i++ {
+		go sayHello(ch, wg)
+		ch <- "Hello world "
+	}
+	wg.Wait()
+}
+
+func r() {
+	go r()
+}
+-----------------------------------------------------------
+
+// I want this program to print "Hello world!", but it doesn't work.
+func main() {
+	ch := make(chan string)
+	done := make(chan bool)
+	go sayHello(ch,done)
+	ch <- "Hello World!"
+	//wait for goroutine to finish
+	time.Sleep(0.5 * time.Second)
+}
+-----------------------------------------------------------
+
+
+func sayHello(ch chan string,done chan bool) {
+	fmt.Println(<-ch)
+	done <- true
+}
+
+func main() {
+	ch := make(chan string)
+	go sayHello(ch)
+	ch <- "Hello World!"
+	if done == true {
+		return
+	} else {
+		log.Fatal("Something went wrong.")
+	}
+}
+
+-----------------------------------------------------------
